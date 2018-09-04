5006bc6ff28857beadd74e1599080d47db5c054f
jbukoski <jacob.bukoski@yale.edu>
2018-09-04 14:46:52 -0700

    Updated pre-processing with streamlined code; outputs single rasters with LSAT bands 1:6 + TC transforms, indices & texture layers.
    Deleted build_svm_tc.R.
    
    Wrote build_svm.R given input rasters with TC data embedded in them.

--- src/build_svm.R
+++ src/build_svm.R
@@ -3,9 +3,13 @@
 
 # Input files are preprocessed landsat images (see lsat_preproceesing.R)
 
+library(cluster)
 library(e1071)
-library(RStoolbox)
+library(randomForest)
 library(raster)
+library(rgdal)
+library(rgeos)
+library(RStoolbox)
 library(sf)
 library(sp)
 library(tidyverse)
@@ -32,18 +36,20 @@ seed <- round(runif(1, 1, 100000))
 
 #----------------------
 # Load in data
+# 1. Tasseled cap transformed LSAT data (for training "year")
+# 2. Tasseled cap transformed LSAT validation data (for validation "year")
+# 3. Training polygons
+# 4. Validation points
+
+band_names <- c("blue", "green", "red", "nir", "swir1", "swir2", "srtm",
+                "brightness", "greenness", "wetness", "ndvi", "ndwi",
+                "avg3", "avg5", "avg9", "var3", "var5", "var9")
 
 lsat <- brick(paste0(in_dir, year_train, site, "lsat.tif"))
-names(lsat) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
+names(lsat) <- band_names
 
 lsat_valid <- brick(paste0(in_dir, year_valid, site, "lsat.tif"))
-names(lsat_valid) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
-
-lsat_tc <- brick(paste0(in_dir, year_train, site, "lsat_tc.tif"))
-names(lsat_tc) <- c("brightness", "greenness", "wetness", "srtm")
-
-lsat_tc_valid <- brick(paste0(in_dir, year_valid, site, "lsat_tc.tif"))
-names(lsat_tc_valid) <- c("brightness", "greenness", "wetness", "srtm")
+names(lsat_valid) <- band_names
 
 polys <- read_sf(paste0(in_dir, year_train, site, "training.shp")) %>% 
   mutate(class_fct = as.numeric(as.factor(class))) %>%
@@ -51,8 +57,10 @@ polys <- read_sf(paste0(in_dir, year_train, site, "training.shp")) %>%
   spTransform(crs(lsat)) %>%
   st_as_sf()
 
-polys_valid <- read_sf(paste0(in_dir, year_valid, site, "training.shp")) %>% 
-  mutate(class_fct = as.numeric(as.factor(class))) %>%
+validation_pts <- read_sf(paste0(in_dir, site, "reg_vld_pts.shp")) %>%
+  mutate(class = ifelse(class == "agricultur", "agriculture", 
+                        ifelse(class == "aquacultur", "aquaculture", class)),
+         class_fct = as.numeric(factor(class))) %>%
   as("Spatial") %>%
   spTransform(crs(lsat))
 
@@ -65,7 +73,7 @@ train_idx <- sample.int(nrow(polys), nrow(polys)*split)
 training <- polys[train_idx, ]
 valid <- polys[-train_idx, ]
 
-polys_spdf <- as(valid, "Spatial")   #Need SPDF for validateMap()
+#polys_spdf <- as(valid, "Spatial")   #Need SPDF for validateMap()
 
 #------------------------------#
 # Classification for LSAT data #
@@ -75,102 +83,141 @@ rast <- raster::extract(lsat, training)
 names(rast) <- training$class
 
 train_df <- plyr::ldply(rast, rbind)
-colnames(train_df) <- c("class", "B1", "B2", "B3", "B4", "B5", 
-                        "B7", "srtm")
+colnames(train_df) <- c("class", band_names)
 train_df$class <- as.factor(train_df$class)
 
-svm_lsat <- svm(class ~ B2 + B3 + B4 + srtm, 
-                data = train_df, cost = 100, gamma = 1)
+svm_lsat <- svm(class ~ ndvi + srtm + brightness + greenness + avg3, 
+                   data = train_df, cost = 100, gamma = 1)
 
 #---------------------
-# LSAT: Run prediction and visualize
+# TC: Run prediction on training year and visualize
 
 lsat_pred <- raster::predict(lsat, svm_lsat)
 values(lsat_pred) <- as.numeric(values(lsat_pred))
 
-plot(lsat_pred, main = paste0(year_train, "training"))
-
 #-----------------------
 # Build water mask
 # Mask B flips land/water == 0 class to remove water speckles
-
-mat_a <- matrix(c(0, 4, 0, 4.9, 5.1, 1), ncol = 3, byrow = TRUE)
-
-lsat_mask_a <- reclassify(lsat_pred, mat_a) %>%
-  rm_speckling(speckle_size = 5, dir = 4)
-
-mat_b <- matrix(c(-0.1, 0.1, 1, 0.9, 1.1, 0), ncol = 3, byrow = TRUE)
-
-lsat_mask_b <- reclassify(lsat_mask_a, mat_b) %>%
-  rm_speckling(speckle_size = 10, dir = 4)
-
-#----------------------
-# Apply water mask
-
-mat_c <- matrix(c(-0.1, 0.1, NA, 0.9, 1.1, 1), ncol = 3, byrow = TRUE)
-lsat_water_mask <- reclassify(lsat_mask_b, mat_c)
-
-masked_pred <- mask(lsat_pred, lsat_water_mask)
-
-mat_d <- matrix(c(0.9, 1.1, 1, 
-                1.9, 2.1, 2,
-                2.9, 3.1, 3,
-                3.9, 4.1, 4,
-                4.9, 5.1, 2,
-                NA, NA, 5), ncol = 3, byrow = TRUE)
-
-final_lsat <- reclassify(masked_pred, mat_d)
+# 
+# mat_a <- matrix(c(0, 4, 0, 4.9, 5.1, 1), ncol = 3, byrow = TRUE)
+# 
+# lsat_mask_a <- reclassify(lsat_pred, mat_a) %>%
+#   rm_speckling(speckle_size = 5, dir = 4)
+# 
+# mat_b <- matrix(c(-0.1, 0.1, 1, 0.9, 1.1, 0), ncol = 3, byrow = TRUE)
+# 
+# lsat_mask_b <- reclassify(lsat_mask_a, mat_b) %>%
+#   rm_speckling(speckle_size = 10, dir = 4)
+# 
+# #----------------------
+# # Apply water mask
+# 
+# mat_c <- matrix(c(-0.1, 0.1, NA, 0.9, 1.1, 1), ncol = 3, byrow = TRUE)
+# lsat_water_mask <- reclassify(lsat_mask_b, mat_c)
+# 
+# mat_d <- matrix(c(0.9, 1.1, 1, 
+#                   1.9, 2.1, 2,
+#                   2.9, 3.1, 3,
+#                   3.9, 4.1, 4,
+#                   4.9, 5.1, 2,
+#                   NA, NA, 5), ncol = 3, byrow = TRUE)
+# 
+# 
+# masked_pred <- mask(lsat_pred, lsat_water_mask)
+# final_lsat <- reclassify(masked_pred, mat_d)
+# 
+# #---------------------------
+# # Visualize the two
+# 
+# par(mfrow = c(1, 2))
+# plot(lsat_pred, main = "w/o mask")
+# plot(final_lsat, main = "w/ mask")
 
 #-----------------------------------
-# Validation & visualize
+# Run prediction on validation year
 
-validateMap(final_lsat, polys_spdf, responseCol = 'class_fct', 
+pred_valid <- raster::predict(lsat_valid, svm_lsat)
+values(pred_valid) <- as.numeric(values(pred_valid))
+
+par(mfrow = c(1,2))
+
+plot(lsat_pred, main = "2017 predicted")
+plot(pred_valid, main = "2007 predicted")
+
+#-----------------------------------
+# Accuracy assessment
+
+accuracy <- validateMap(lsat_pred, validation_pts, 
+                          responseCol = 'class_fct', 
+                          1000, mode = "classification")
+
+acc_mat <- matrix(accuracy$performance$table, nrow = 5, ncol = 5)
+
+users <- diag(acc_mat) / rowSums(acc_mat)
+producers <- diag(acc_mat) / colSums(acc_mat)
+
+plot(pred_valid, main = paste0(year_valid, "validation"))
+plot(validation_pts, add = T)
+
+#-----------------------------#
+# Unsupervised classification #
+#-----------------------------#
+# Using kmeans clustering
+
+v <- getValues(lsat_valid)
+i <- which(!is.na(v))
+kmeans_rast <- kmeans(v[i], 12, iter.max = 100, nstart = 10)
+
+kmeans_raster <- raster(lsat)
+kmeans_raster[i] <- kmeans_rast$cluster
+plot(kmeans_raster)
+
+kmeans_rc_mat <- matrix(c(0.9, 1.1, 4, 
+                          1.9, 2.1, 2,
+                          2.9, 3.1, 3,
+                          3.9, 4.1, 1,
+                          4.9, 5.1, 5), ncol = 3, byrow = TRUE)
+
+kmeans_raster <- reclassify(kmeans_raster, kmeans_rc_mat)
+
+validateMap(kmeans_raster, validation_pts, responseCol = 'class_fct', 
             1000, mode = "classification")
 
-plot(final_lsat, main = paste0(year_train, "training w/ water mask"))
+plot(lsat_pred)
 
-#-----------------------------------
-# Validate with validation year
-# 1. Apply model prediction to 2007 data
-# 2. Use 2007 training data to validate the prediction
+# Unsupervised classification using randomForest classification w/ kmeans
 
-lsat_pred_valid <- raster::predict(lsat_valid, svm_lsat)
-values(lsat_pred_valid) <- as.numeric(values(lsat_pred_valid))
+vx <- v[sample(nrow(v), 500), ]
+rf <- randomForest(vx)
+rf_prox <- randomForest(vx, ntree = 1000, proximity = TRUE)$proximity
 
-validateMap(lsat_pred_valid, polys_valid, responseCol = 'class_fct', 
-            1000, mode = "classification")
+E_rf <- kmeans(rf_prox, 5, iter.max = 100, nstart = 10)
+rf <- randomForest(vx, as.factor(E_rf$cluster), ntree = 500)
+rf_raster <- predict(lsat_valid, rf)
+plot(rf_raster)
 
-plot(lsat_pred_valid, main = paste0(year_valid, "validation"))
-
-#-----------------------------------
-# Predict for all years
+#----------------------------------
+# Load all layers and run predictions on them
 
 lsat1987 <- brick(paste0(in_dir, "1987_", site, "lsat.tif"))
-names(lsat1987) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
+names(lsat1987) <- band_names
 
 lsat1997 <- brick(paste0(in_dir, "1997_", site, "lsat.tif"))
-names(lsat1997) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
+names(lsat1997) <- band_names
 
 lsat2007 <- brick(paste0(in_dir, "2007_", site, "lsat.tif"))
-names(lsat2007) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
+names(lsat2007) <- band_names
 
 lsat2017 <- brick(paste0(in_dir, "2017_", site, "lsat.tif"))
-names(lsat2017) <- c("B1", "B2", "B3", "B4", "B5", "B7", "srtm")
+names(lsat2017) <- band_names
 
-pred1987 <- raster::predict(lsat1987, svm_lsat)
-values(pred1987) <- as.numeric(values(pred1987))
-pred1997 <- raster::predict(lsat1997, svm_lsat)
-pred2007 <- raster::predict(lsat2007, svm_lsat)
-pred2017 <- raster::predict(lsat2017, svm_lsat)
+pred_1987 <- raster::predict(lsat1987, svm_lsat)
+pred_1997 <- raster::predict(lsat1997, svm_lsat)
+pred_2007 <- raster::predict(lsat2007, svm_lsat)
+pred_2017 <- raster::predict(lsat2017, svm_lsat)
 
 par(mfrow = c(2,2))
-plot(pred1987, main = "1987")
-plot(pred1997, main = "1997")
-plot(pred2007, main = "2007")
-plot(pred2017, main = "2017")
-
-#-----------------------------------
-# Write out LSAT raster
-
-# writeRaster(final_lsat, paste0(out_dir, "prcssd_", year, site, "lsat.tif"),
-#             format = "GTiff", overwrite = TRUE)
+plot(pred_1987, main = '1987')
+plot(pred_1997, main = '1997')
+plot(pred_2007, main = '2007')
+plot(pred_2017, main = '2017')
--- src/lsat_preprocessing.R
+++ src/lsat_preprocessing.R
@@ -21,67 +21,44 @@ out_dir <- "/home/jbukoski/research/data/thailand_stocks/output/"
 
 year <- "2017_"
 site <- "krabi_"
-sensor <- "OLI" # Enter 4TM, 5TM or OLI depending on input data sensor
+
+# Enter 4TM, 5TM or OLI depending on input data sensor
+
+if(year == "1987_") {
+  sensor <- "Landsat4TM"
+} else if(year == "2017_") {
+  sensor <- "Landsat8OLI"
+} else {
+  sensor <- "Landsat5TM"
+}
 
 #----------------------
 # Load in data:
 # 1. LSAT data for site and year of interest. Output from GEE script.
 # 2. SRTM data for the site of interest.
 
-lsat <- brick(paste0(in_dir, "matched_", year, site, "lsat.tif"))
+lsat <- stack(paste0(in_dir, "matched_", year, site, "lsat.tif"))
 
 names(lsat) <- c("blue", "green", "red", "nir", "swir1", "swir2", "srtm")
 
 #----------------------
-# Tasselled cap processing (B1: Brightness, B2: Greenness, B3: Wetness)
+# Generate Tasseled Cap transformation, NDVI and NDWI indices
 # Note - coefficients are sensor specific, be careful of which LSAT mission
 
-if(sensor == "4TM") {
-  lsat_tc <- tasseledCap(lsat[[c(1:6)]], "Landsat4TM")
-  
-  ndvi <- ( lsat[[4]] - lsat[[3]] ) / ( lsat[[4]] + lsat[[3]] )
-  rc_mat <- matrix(c(1, 100, 1, -100, -1, -1), nrow = 2, ncol = 3, byrow = T)
-  ndvi <- reclassify(ndvi, rc_mat)
-  
-  ndwi <- ( lsat[[2]] - lsat[[4]] ) / ( lsat[[2]] + lsat[[4]] )  # McFeeters NDWI 1996
-  ndwi <- reclassify(ndwi, rc_mat)
-  
-  lsat_tc[[4]] <- lsat[[7]]
-  lsat_tc[[5]] <- ndvi
-  lsat_tc[[6]] <- ndwi
+lsat_tc <- tasseledCap(lsat[[c(1:6)]], sensor)
 
-} else if(sensor == "5TM") {
-  lsat_tc <- tasseledCap(lsat[[c(1:6)]], "Landsat5TM")
-  
-  ndvi <- ( lsat[[4]] - lsat[[3]] ) / ( lsat[[4]] + lsat[[3]] )
-  rc_mat <- matrix(c(1, 100, 1, -100, -1, -1), nrow = 2, ncol = 3, byrow = T)
-  ndvi <- reclassify(ndvi, rc_mat)
-  
-  ndwi <- ( lsat[[2]] - lsat[[4]] ) / ( lsat[[2]] + lsat[[4]] )  # McFeeters NDWI 1996
-  ndwi <- reclassify(ndwi, rc_mat)
-  
-  lsat_tc[[4]] <- lsat[[7]]
-  lsat_tc[[5]] <- ndvi
-  lsat_tc[[6]] <- ndwi
-  
-} else if(sensor == "OLI") {
-  lsat_tc <- tasseledCap(lsat[[c(1:6)]], "Landsat8OLI")
-  
-  ndvi <- ( lsat[[5]] - lsat[[4]] ) / ( lsat[[5]] + lsat[[4]] )
-  rc_mat <- matrix(c(1, 100, 1, -100, -1, -1), nrow = 2, ncol = 3, byrow = T)
-  ndvi <- reclassify(ndvi, rc_mat)
-  
-  ndwi <- ( lsat[[3]] - lsat[[5]] ) / ( lsat[[3]] + lsat[[5]] )  # McFeeters NDWI 1996
-  ndwi <- reclassify(ndwi, rc_mat)
-  
-  lsat_tc[[4]] <- lsat[[7]]
-  lsat_tc[[5]] <- ndvi
-  lsat_tc[[6]] <- ndwi
-  
-}
+ndvi <- ( lsat[[4]] - lsat[[3]] ) / ( lsat[[4]] + lsat[[3]] )
+rc_mat <- matrix(c(1, 100, 1, -100, -1, -1), nrow = 2, ncol = 3, byrow = T)
+ndvi <- reclassify(ndvi, rc_mat)
+
+ndwi <- ( lsat[[2]] - lsat[[4]] ) / ( lsat[[2]] + lsat[[4]] )  # McFeeters NDWI 1996
+ndwi <- reclassify(ndwi, rc_mat)
+
+lsat <- stack(lsat, lsat_tc, ndvi, ndwi)
+names(lsat) <- c(names(lsat)[1:10], 'ndvi', 'ndwi')
 
 #--------------------------
-# Texture metrics
+# Generate texture metrics
 
 # Specify window sizes
 threes <- matrix(1, nrow = 3, ncol = 3)
@@ -97,21 +74,13 @@ ndvi_var_3 <- focal(ndvi, w = threes, fun = var)
 ndvi_var_5 <- focal(ndvi, w = fives, fun = var)
 ndvi_var_9 <- focal(ndvi, w = nines, fun = var)
 
-lsat_tc[[7]] <- ndvi_avg_3
-lsat_tc[[8]] <- ndvi_avg_5
-lsat_tc[[9]] <- ndvi_avg_9
-lsat_tc[[10]] <- ndvi_var_3
-lsat_tc[[11]] <- ndvi_var_5
-lsat_tc[[12]] <- ndvi_var_9
+texture_names <- c(names(lsat), "avg3", "avg5", "avg9", "var3", "var5", "var9")
 
-names(lsat_tc) <- c("brightness", "greenness", "wetness", "srtm", "ndvi", "ndwi",
-                    "avg3", "avg5", "avg9", "var3", "var5", "var9")
+lsat <- stack(lsat, ndvi_avg_3, ndvi_avg_5, ndvi_avg_9, ndvi_var_3, ndvi_var_5, ndvi_var_9)
+names(lsat) <- texture_names
 
 #----------------------
 # Write out files to in_dir for feeding into build_classification scripts
 
 writeRaster(lsat, paste0(in_dir, year, site, "lsat.tif"),
-            format = "GTiff", overwrite = TRUE)
-
-writeRaster(lsat_tc, paste0(in_dir, year, site, "lsat_tc.tif"), 
             format = "GTiff", overwrite = TRUE)
