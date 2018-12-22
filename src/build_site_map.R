88ba36be229fab128f4c05c8479abe79cd720c38
jbukoski <jacob.bukoski@yale.edu>
2018-12-22 16:48:25 +0700

    Updating build_site_map.R

--- src/build_site_map.R
+++ src/build_site_map.R
@@ -1,11 +1,14 @@
 # Script for creation of a site map for stocks study
 
+library(cowplot)
 library(ggmap)
 library(ggplot2)
+library(ggthemes)
+library(gridExtra)
 library(rgdal)
 library(sf)
-library(ggthemes)
-library(cowplot)
+
+
 
 in_dir <- "/home/jbukoski/research/data/thailand_stocks/input/site_map/"
 
@@ -14,40 +17,46 @@ thailand <- read_sf(paste0(in_dir, "thailand_boundary.shp"))
 mangroves <- read_sf(paste0(in_dir, "thailand_mangroves.shp"))
 krabi <- read_sf(paste0(in_dir, "krabi_admin.shp"))
 nakorn <- read_sf(paste0(in_dir, "nakorn_admin.shp"))
+trang <- read_sf(paste0(in_dir, "trang_admin.shp"))
 kre <- read_sf(paste0(in_dir, "kre_boundary.shp"))
 ppm <- read_sf(paste0(in_dir, "ppm_boundary.shp"))
+pre <- read_sf(paste0(in_dir, "pre_boundary.shp"))
 
 p1 <- ggplot(se_asia) +
   geom_sf(data = se_asia, fill = "#f9f0f9") +
   geom_sf(data = thailand, fill = "#cacaca") +
-  geom_sf(data = krabi, fill = "yellow", lwd = 0.1) +
-  geom_sf(data = nakorn, fill = "yellow", lwd = 0.1) +
   geom_sf(data = mangroves, fill = "dark green", col = "dark green") +
+  geom_sf(data = krabi, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
+  geom_sf(data = nakorn, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
+  geom_sf(data = trang, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
+  #geom_sf(data = kre, fill = NA, col = "red", lwd = 1.1) +
+  #geom_sf(data = ppm, fill = NA, col = "red", lwd = 1.1) +
+  #geom_sf(data = pre, fill = NA, col = "red", lwd = 1.1) +
   theme_tufte() +
-  xlim(c(90, 110)) + 
-  ylim(c(0, 25)) +
-  theme(panel.border = element_rect(colour = "black", fill = NA),
-        plot.margin = unit(c(0, -1.5, 0, 0), "cm"))
-  
-p1
+  xlim(c(95, 107)) + 
+  ylim(c(5, 22)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA))
 
 p2 <- ggplot(thailand) +
   geom_sf(data = thailand, fill = "#cacaca") +
-  geom_sf(data = krabi, fill = "yellow", lwd = 0.1) +
-  geom_sf(data = nakorn, fill = "yellow", lwd = 0.1) +
+  geom_sf(data = krabi, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
+  geom_sf(data = nakorn, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
+  geom_sf(data = trang, col = "black", fill = NA, lwd = 0.25, linetype = "dashed") +
   geom_sf(data = mangroves, fill = "dark green", col = "dark green") +
   geom_sf(data = kre, fill = NA, col = "red", lwd = 1.1) +
   geom_sf(data = ppm, fill = NA, col = "red", lwd = 1.1) +
+  geom_sf(data = pre, fill = NA, col = "red", lwd = 1.1) +
+  geom_text(y = 8.2,  x = 99, label = "A", size = 4) +
+  geom_text(y = 8.2,  x = 100.1, label = "B", size = 4) +
+  geom_text(y = 7.5,  x = 99.5, label = "C", size = 4) +
   theme_tufte() +
   xlim(c(97.75, 100.75)) +
-  ylim(c(7.5, 9.25)) +
-  theme(panel.border = element_rect(colour = "black", fill = NA),
-        plot.margin = unit(c(0, 0, 0, -1), "cm"))
+  ylim(c(7.0, 9.3)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA))
   
-p2
+g1 <- ggplotGrob(p1)
+g2 <- ggplotGrob(p2)
 
-grid.arrange(p1, p2, nrow = 1, respect = TRUE, widths = c(1, 2), padding = 0)
+grid.arrange(g1, g2, ncol = 2, respect = TRUE, widths = c(0.75, 1.29))
 
-plot_grid(p1, p2, align = "h", rel_widths = c(1, 2))
-
-arrangeGrob(p1, p2)
+  
